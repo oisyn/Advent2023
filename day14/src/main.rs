a1933b821433bcc770c768d32a3e9b3137cea56e
Sylvester Hesp <github@oisyn.nl>
2023-12-14 22:59:35 +0100

    alternative solution for day14

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,153 +1,102 @@
 #![allow(dead_code)]
 
-use std::{arch::x86_64::_pext_u64, collections::HashMap};
+use std::collections::HashMap;
 
 use anyhow::Result;
 use util::*;
-
-fn as_bytes_mut<T>(data: &mut [T]) -> &mut [u8] {
-    unsafe {
-        std::slice::from_raw_parts_mut(
-            data.as_mut_ptr() as *mut u8,
-            data.len() * std::mem::size_of::<T>(),
-        )
-    }
-}
-
-fn transpose(data: &mut [u128]) {
-    fn read8x8(source: &[u8]) -> u64 {
-        let mut s = 0_u64;
-        for i in 0..8 {
-            s |= (source[i * 16] as u64) << (i * 8);
-        }
-        s
-    }
-
-    fn write8x8(s: u64, dest: &mut [u8]) {
-        const SOURCE_PATTERN: u64 = 0x01010101_01010101;
-
-        for i in 0..8 {
-            dest[i * 16] = unsafe { _pext_u64(s, SOURCE_PATTERN << i) as u8 };
-        }
-    }
-
-    let data = as_bytes_mut(data);
-
-    for y in 0..16 {
-        let s = read8x8(&data[y * 128 + y..]);
-        write8x8(s, &mut data[y * 128 + y..]);
-
-        for x in y + 1..16 {
-            let s0 = read8x8(&data[y * 128 + x..]);
-            let s1 = read8x8(&data[x * 128 + y..]);
-            write8x8(s0, &mut data[x * 128 + y..]);
-            write8x8(s1, &mut data[y * 128 + x..]);
-        }
-    }
-}
-
-fn flip(data: &mut [u128], h: usize) {
-    for y in 0..h / 2 {
-        (data[y], data[h - y - 1]) = (data[h - y - 1], data[y]);
-    }
-}
-
-fn rotate_cw(dest: &mut [u128], h: usize) {
-    flip(dest, h);
-    transpose(dest);
-}
-
-fn fall_north(rocks: &mut [u128], fixed: &[u128], occupied: &mut [u128], h: usize) {
-    for mut y in 0..h {
-        occupied[y] = fixed[y];
-
-        let mut cur = rocks[y];
-        rocks[y] = 0;
-        while y > 0 && cur != 0 {
-            let immovable = cur & occupied[y - 1];
-            occupied[y] |= immovable;
-            rocks[y] |= immovable;
-            y -= 1;
-            cur ^= immovable;
-        }
-
-        if cur > 0 {
-            occupied[0] |= cur;
-            rocks[0] |= cur;
-        }
-    }
-}
-
-fn weight(rocks: &[u128], h: usize) -> u32 {
-    let mut total = 0;
-
-    for y in 0..h {
-        total += (h - y) as u32 * rocks[y].count_ones();
-    }
-
-    total
-}
-
-fn show(rocks: &[u128], fixed: &[u128], w: usize, h: usize) {
-    for y in 0..h {
-        for x in 0..w {
-            if rocks[y] & (1 << x) != 0 {
-                print!("O");
-            } else if fixed[y] & (1 << x) != 0 {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!("");
-    }
-}
 
 fn main() -> Result<()> {
     let input = open_input("day14")?;
     let data = input.bytes();
 
     let width = data.iter().position(|&b| b == b'\r' || b == b'\n').unwrap();
-    assert!(width < 128);
     let stride = width + 1 + (data[width + 1] == b'\n') as usize;
     let height = (data.len() + 2) / stride;
     let field = FieldView::new(data, width, stride, height);
-    let mut rocks = vec![0_u128; 128];
-    let mut fixed_n = vec![0_u128; 128];
-    let mut occupied = vec![0_u128; 128];
+    let mut rocks = Vec::with_capacity(10000);
+    let mut fences = Vec::with_capacity(10000);
 
     let mut total1 = 0;
     let mut total2 = 0;
+    let mut weight = 0;
 
+    let mut field_w = vec![0_u32; width * height];
     for (y, row) in field.rows().enumerate() {
-        let mut r: u128 = 0_u128;
-        let mut f = 0_u128;
+        let mut fence_idx = fences.len() as u32;
+        let o = (y * width) as u32;
+        fences.push((o.wrapping_sub(1), [0, 0, 0, 0]));
+
         for x in 0..width {
+            let o = o + x as u32;
             match row[x] {
-                b'O' => r |= 1_u128 << x,
-                b'#' => f |= 1_u128 << x,
+                b'O' => rocks.push(o),
+                b'#' => {
+                    fence_idx = fences.len() as u32;
+                    fences.push((o, [0, 0, 0, 0]));
+                }
                 _ => {}
             }
+            field_w[o as usize] = fence_idx;
         }
-        rocks[y] = r;
-        fixed_n[y] = f;
     }
 
-    let mut fixed_w = fixed_n.clone();
-    rotate_cw(&mut fixed_w, height);
+    let mut field_n = vec![0_u32; width * height];
+    for x in 0..width {
+        let mut fence_idx = fences.len() as u32;
+        let o = x as u32;
+        fences.push((o.wrapping_sub(width as u32), [0, 0, 0, 0]));
+        for (y, &b) in field.col(x).into_iter().enumerate() {
+            let o = (y * width) as u32 + o;
+            if b == b'#' {
+                fence_idx = field_w[o as usize];
+            }
+            field_n[o as usize] = fence_idx;
+        }
+    }
 
-    let mut fixed_s = fixed_w.clone();
-    rotate_cw(&mut fixed_s, width);
+    let mut field_e = vec![0_u32; width * height];
+    for y in 0..height {
+        let mut fence_idx = fences.len() as u32;
+        let o = (y * width) as u32;
+        fences.push((o + width as u32, [0, 0, 0, 0]));
+        for (x, &b) in field.row(y).into_iter().enumerate().rev() {
+            let o = o + x as u32;
+            if b == b'#' {
+                fence_idx = field_w[o as usize];
+            }
+            field_e[o as usize] = fence_idx;
+        }
+    }
 
-    let mut fixed_e = fixed_s.clone();
-    rotate_cw(&mut fixed_e, height);
+    let mut field_s = vec![0_u32; width * height];
+    for x in 0..width {
+        let mut fence_idx = fences.len() as u32;
+        let o = x as u32;
+        fences.push((o + (width * height) as u32, [0, 0, 0, 0]));
+        for (y, &b) in field.col(x).into_iter().enumerate().rev() {
+            let o = (y * width) as u32 + o;
+            if b == b'#' {
+                fence_idx = field_w[o as usize];
+            }
+            field_s[o as usize] = fence_idx;
+        }
+    }
 
-    let mut history: HashMap<_, _> = HashMap::new();
+    for &r in &rocks {
+        let mut o;
+        o = field_n[r as usize];
+        fences[o as usize].1[0] += 1;
+        o = fences[o as usize]
+            .0
+            .wrapping_add(fences[o as usize].1[0] * width as u32);
+        total1 += height as u32 - (o / width as u32);
+    }
+
+    let mut history = HashMap::new();
 
     for cycle in 0..1_000_000_000 {
-        let (prev, _) = *history
-            .entry(rocks.clone())
-            .or_insert_with(|| (cycle, weight(&rocks, height)));
+        rocks.sort();
+        let (prev, _) = *history.entry(rocks.clone()).or_insert((cycle, weight));
         if prev < cycle {
             let len = cycle - prev;
             let d = (1_000_000_000 - prev) % len + prev;
@@ -158,21 +107,37 @@ fn main() -> Result<()> {
             break;
         }
 
-        fall_north(&mut rocks, &fixed_n, &mut occupied, height);
-        if cycle == 0 {
-            total1 = weight(&rocks, height);
+        for f in &mut fences {
+            f.1 = [0, 0, 0, 0];
         }
 
-        rotate_cw(&mut rocks, height);
-        fall_north(&mut rocks, &fixed_w, &mut occupied, width);
+        weight = 0;
 
-        rotate_cw(&mut rocks, width);
-        fall_north(&mut rocks, &fixed_s, &mut occupied, height);
+        for r in &mut rocks {
+            let mut o;
+            o = field_n[*r as usize];
+            fences[o as usize].1[0] += 1;
+            o = fences[o as usize]
+                .0
+                .wrapping_add(fences[o as usize].1[0] * width as u32);
 
-        rotate_cw(&mut rocks, height);
-        fall_north(&mut rocks, &fixed_e, &mut occupied, width);
+            o = field_w[o as usize];
+            fences[o as usize].1[1] += 1;
+            o = fences[o as usize].0.wrapping_add(fences[o as usize].1[1]);
 
-        rotate_cw(&mut rocks, width);
+            o = field_s[o as usize];
+            fences[o as usize].1[2] += 1;
+            o = fences[o as usize]
+                .0
+                .wrapping_sub(fences[o as usize].1[2] * width as u32);
+
+            o = field_e[o as usize];
+            fences[o as usize].1[3] += 1;
+            o = fences[o as usize].0.wrapping_sub(fences[o as usize].1[3]);
+
+            *r = o;
+            weight += height as u32 - (o / width as u32);
+        }
     }
 
     drop(input);
