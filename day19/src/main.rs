ad9987d8fdc1c29009c712180c22838e18f611cc
Sylvester Hesp <github@oisyn.nl>
2023-12-20 01:25:57 +0100

    day19 optimization

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -3,52 +3,88 @@ use std::collections::HashMap;
 use anyhow::Result;
 use util::*;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum Action<'a> {
-    Accept,
-    Reject,
-    Goto(&'a str),
+#[derive(Clone, Copy, Debug)]
+struct Rule<'a> {
+    action: &'a str,
+    t: u8,
+    c: u8,
+    v: u32,
 }
 
-#[derive(Clone, Copy, Debug)]
-enum Rule<'a> {
-    Direct(Action<'a>),
-    Smaller(u8, u32, Action<'a>),
-    Larger(u8, u32, Action<'a>),
+impl<'a> Rule<'a> {
+    fn direct(action: &'a str) -> Self {
+        Self {
+            action,
+            t: 0,
+            c: 0,
+            v: 0,
+        }
+    }
+
+    fn compare(action: &'a str, t: u8, c: u8, v: u32) -> Self {
+        Self { action, t, c, v }
+    }
 }
 
 impl<'p> FromParser<'p> for Rule<'p> {
     fn parse_from(p: &mut Parser<'p>) -> Option<Self> {
         let n = p.take_while(|c| !b"<>,}".contains(&c));
         if n.len() > 1 {
-            return Some(Rule::Direct(Action::Goto(n)));
+            return Some(Rule::direct(n));
         }
         let b = n.as_bytes()[0];
-        if b == b'A' {
-            return Some(Rule::Direct(Action::Accept));
-        } else if b == b'R' {
-            return Some(Rule::Direct(Action::Reject));
-        }
         if {
             let pc = p.peek_char().unwrap();
             pc == b',' || pc == b'}'
         } {
-            return Some(Rule::Direct(Action::Goto(n)));
+            return Some(Rule::direct(n));
         }
 
         let b = b"xmas".iter().position(|&c| b == c)? as u8;
         let c = p.take_char()?;
         let num = p.parse::<u32>()?;
-        let action = match p.expect(":").take_while(|c| c != b'}' && c != b',') {
-            "A" => Action::Accept,
-            "R" => Action::Reject,
-            n => Action::Goto(n),
-        };
+        let action = p.expect(":").take_while(|c| c != b'}' && c != b',');
 
-        if c == b'<' {
-            Some(Rule::Smaller(b, num, action))
-        } else {
-            Some(Rule::Larger(b, num, action))
+        Some(Rule::compare(action, c, b, num))
+    }
+}
+
+fn optimize(workflows: &mut HashMap<&str, Vec<Rule>>) {
+    let mut rename = HashMap::new();
+
+    loop {
+        let mut changed = false;
+
+        for &name in rename.keys() {
+            if name != "in" {
+                workflows.remove(name);
+            }
+        }
+
+        for (&name, rules) in workflows.iter_mut() {
+            for r in rules.iter_mut() {
+                while let Some(&new) = rename.get(r.action) {
+                    r.action = new;
+                }
+            }
+
+            let last = rules[rules.len() - 1].action;
+            while rules.len() > 1 {
+                if rules[rules.len() - 2].action == last {
+                    rules.remove(rules.len() - 2);
+                } else {
+                    break;
+                }
+            }
+
+            if rules.len() == 1 {
+                rename.insert(name, last);
+                changed = true;
+            }
+        }
+
+        if !changed {
+            break;
         }
     }
 }
@@ -75,6 +111,8 @@ fn main() -> Result<()> {
         workflows.insert(name, rules);
     }
 
+    optimize(&mut workflows);
+
     let mut total1 = 0;
 
     for l in lines {
@@ -91,13 +129,25 @@ fn main() -> Result<()> {
             let action = 'action: {
                 for &r in workflow {
                     match r {
-                        Rule::Direct(a) => break 'action a,
-                        Rule::Smaller(c, v, a) => {
+                        Rule {
+                            action: a, t: 0, ..
+                        } => break 'action a,
+                        Rule {
+                            action: a,
+                            t: b'<',
+                            c,
+                            v,
+                        } => {
                             if part[c as usize] < v {
                                 break 'action a;
                             }
                         }
-                        Rule::Larger(c, v, a) => {
+                        Rule {
+                            action: a,
+                            t: _,
+                            c,
+                            v,
+                        } => {
                             if part[c as usize] > v {
                                 break 'action a;
                             }
@@ -107,39 +157,46 @@ fn main() -> Result<()> {
                 unreachable!();
             };
 
-            workflow = match action {
-                Action::Accept => {
-                    total1 += part.iter().sum::<u32>();
-                    break;
-                }
-                Action::Reject => break,
-                Action::Goto(wf) => &workflows[wf],
+            if action == "A" {
+                total1 += part.iter().sum::<u32>();
+                break;
             }
+            if action == "R" {
+                break;
+            }
+            workflow = &workflows.get(action).expect(action);
         }
     }
 
     let mut total2 = 0;
 
     let mut queue = Vec::with_capacity(10000);
-    queue.push(([(1, 4000); 4], Action::Goto("in")));
+    queue.push(([(1, 4000); 4], "in"));
     while !queue.is_empty() {
         let (mut part, mut action) = queue.pop().unwrap();
 
         loop {
-            let workflow = match action {
-                Action::Accept => {
-                    total2 += part.iter().fold(1_u64, |s, r| s * (r.1 - r.0 + 1) as u64);
-                    break;
-                }
-                Action::Reject => break,
-                Action::Goto(wf) => &workflows[wf],
-            };
+            if action == "A" {
+                total2 += part.iter().fold(1_u64, |s, r| s * (r.1 - r.0 + 1) as u64);
+                break;
+            }
+            if action == "R" {
+                break;
+            }
+            let workflow = &workflows[action];
 
             action = 'action: {
                 for &r in workflow {
                     match r {
-                        Rule::Direct(a) => break 'action a,
-                        Rule::Smaller(c, v, a) => {
+                        Rule {
+                            action: a, t: 0, ..
+                        } => break 'action a,
+                        Rule {
+                            action: a,
+                            t: b'<',
+                            c,
+                            v,
+                        } => {
                             let r = part[c as usize];
                             if r.1 < v {
                                 break 'action a;
@@ -150,7 +207,12 @@ fn main() -> Result<()> {
                                 part[c as usize] = (v, r.1);
                             }
                         }
-                        Rule::Larger(c, v, a) => {
+                        Rule {
+                            action: a,
+                            t: _,
+                            c,
+                            v,
+                        } => {
                             let r = part[c as usize].clone();
                             if r.0 > v {
                                 break 'action a;
